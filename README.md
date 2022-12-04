0206c819dc391bab0cd30620e93e6ba9b1f2081c
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2022-12-03 21:12:32 -0500

    day 1 ✅

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ My solutions to this year's problems linked [here](https://adventofcode.com/2022
 
 ## Progress
 
-Day | Problem                                                             | Part One   | Part Two   | 
-:-: | :-----------------------------------------------------------------: | :--------: | :--------: | 
+Day | Problem                                                      | Part One   | Part Two   | 
+:-: | :----------------------------------------------------------: | :--------: | :--------: | 
+01  | [Calorie Counting](https://adventofcode.com/2022/day/1)      | 173657     | 167153     | 
+
+
+Auto-Generated at 2022-12-03 21:12:32.095324
---
+++ solutions/day1.py
@@ -0,0 +1,53 @@
+from typing import List, Dict, Set
+from collections import *
+from functools import lru_cache
+from pprint import pprint as pp
+from math import *
+from statistics import *
+from helper.submit import *
+from utils import *
+
+
+DAY = 1
+setup(DAY)
+lines = read_file(DAY)
+
+
+# Part 1
+##################################################
+def part1(lines: List[str]) -> int:
+    lines = read_file(DAY)
+    elves = []
+    elf_sum = 0
+    for line in lines:
+        if line == "":
+            elves.append(elf_sum)
+            elf_sum = 0
+        else:
+            elf_sum += int(line)
+    if elf_sum != 0:
+        elves.append(elf_sum)
+    return max(elves)
+
+submit(1, part1(lines), force=False)
+
+
+# Part 2
+##################################################
+def part2(lines: List[str]) -> int:
+    lines = read_file(DAY)
+    elves = []
+    elf_sum = 0
+    for line in lines:
+        if line == "":
+            elves.append(elf_sum)
+            elf_sum = 0
+        else:
+            elf_sum += int(line)
+    if elf_sum != 0:
+        elves.append(elf_sum)
+    return sum(list(sorted(elves, reverse=True))[0:3])
+
+submit(2, part2(lines), force=False)
+
+
